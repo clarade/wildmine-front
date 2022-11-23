6cc61d5bfda3d13dad0657a28095fd1c01679aa9
JIdayyy <julien_abbadie@hotmail.fr>
2022-11-23 22:13:41 +0100

    server link and features

---
+++ src/pages/components/dashboard/Diagram.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { userWithRelations } from "../../../graphql/UserSession";
+import { useQuery } from "@apollo/client";
+import { Doughnut } from "react-chartjs-2";
+import { Chart, ArcElement } from "chart.js";
+Chart.register(ArcElement);
+
+const Diagram = () => {
+  const { loading, error, data } = useQuery(userWithRelations);
+  // console.log(data);
+
+  const status = data.userWithRelations.issues_assigned;
+
+  const chartData = {
+    inWait: status.filter((status) => status.status === "IN_WAIT").length,
+    inProgress: status.filter((status) => status.status === "IN_PROGRESS")
+      .length,
+    done: status.filter((status) => status.status === "DONE").length,
+  };
+  console.log(status);
+  const datas = {
+    labels: ["Gray", "Yellow", "Yellow"],
+    datasets: [
+      {
+        data: [chartData.inWait, chartData.inProgress, chartData.done],
+        backgroundColor: ["#808080", "#FFFF00", "#008000"],
+        hoverBackgroundColor: ["#00A550", "#F0F0F0", "#EACC41"],
+      },
+    ],
+    options: {
+      // responsive: true,
+      cutoutPercentage: 30,
+    },
+  };
+
+  return (
+    <>
+      {/* <canvas className="diagram-size"> */}
+      {/* <pre>{JSON.stringify(status, null, 2)}</pre> */}
+      <Doughnut data={datas} />
+      <div className="p-4">
+        {/* <img className="w-full" src={diagram} alt="diagram" /> */}
+        <div className="font-bold text-xl mb-2 font-chaney_title text-md text-center">
+          Résumé de mes tickets
+        </div>
+        <div className="px-6 py-4 ">
+          <div className=" grid-cols-2 p-4 flex flex-col">
+            <div>
+              <p className="text_color_light">
+                Non traités ( {chartData.inWait} )
+              </p>
+              <p className="text_color_light">
+                En cours ( {chartData.inProgress} )
+              </p>
+              <p className="text_color_light">Résolus ( {chartData.done} )</p>
+            </div>
+            <div className="my-2">
+              <p className="text_color_light">
+                Nombre de tickets résolus depuis{" "}
+                {new Date(
+                  data.userWithRelations.created_at
+                ).toLocaleDateString()}
+              </p>
+              <div className="font-chaney_title text-center">
+                {chartData.done}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* </canvas> */}
+    </>
+  );
+};
+
+export default Diagram;
---
+++ src/pages/components/projects/DisplayProject.js
@@ -0,0 +1,55 @@
+import React from "react";
+import smiley from "../../../images/smiley.png";
+import { NavLink } from "react-router-dom";
+
+const DisplayProject = ({
+  setDisplayHover,
+  index,
+  displayHover,
+  project,
+  isMobile,
+}) => {
+  console.log(project);
+  return (
+    <div
+      onMouseEnter={() => setDisplayHover(index + 1)}
+      onMouseLeave={() => setDisplayHover(0)}
+      className="organization-project-container"
+      style={{ height: "30rem" }}
+    >
+      <NavLink to={"/detailsProject/" + project.id}>
+        {(isMobile || displayHover === index + 1) && (
+          <div
+            className={`hover-container ${
+              displayHover === index + 1 ? "hover-open" : ""
+            }`}
+          >
+            <p className="font-bold mb-8">
+              {project ? project.name : "Un joli hoover"}
+            </p>
+            <p>
+              {project
+                ? project.description
+                : `C'est l'histoire d'un joli hoover, il était petit et donc pas très grand. Mais un jour il décida de se relevé et devenir un grand hoover!
+                C'est pour cela qu'il décida de grandir et donc d'être grand. Merci d'avoir perdu du temps à lire ce hoover, bisous!`}
+            </p>
+          </div>
+        )}
+
+        <img
+          src={
+            project.images.length > 0
+              ? `${process.env.REACT_APP_SERVER_URL}/images/${
+                  project.images[project.images.length - 1].name
+                }`
+              : smiley
+          }
+          alt="Sélection du projet"
+          className="rounded"
+        />
+      </NavLink>
+    </div>
+  );
+};
+
+export default DisplayProject;
