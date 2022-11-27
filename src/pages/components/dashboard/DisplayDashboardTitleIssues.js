15146bda00790026e091255a0ee304aea14dc735
clarade <cdesperben@yahoo.fr>
2022-11-27 21:50:25 +0100

    trying to fix problem with projects on dashboard

--- src/pages/components/dashboard/DisplayDashboardTitleIssues.js
+++ src/pages/components/dashboard/DisplayDashboardTitleIssues.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const DisplayDashboardTitleIssues = () => (
-	<div className="grid grid-cols-5 p-4 bg-grey_light text-wildmine_black shadow-md rounded-t-lg font-bold mt-12 text-center sm:text-xs">
+	<div className="grid grid-cols-6 p-4 bg-grey_light text-wildmine_black shadow-md rounded-t-lg font-bold mt-12 text-center text-base sm:text-xs">
 		<p>No du ticket</p>
 		<p>Priorité</p>
 		<p>Nom du ticket</p>
