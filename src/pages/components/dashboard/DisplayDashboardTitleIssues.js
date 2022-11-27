c353822e7ce54c8145f7f3a74fd89cb0e38ebfce
clarade <cdesperben@yahoo.fr>
2022-11-27 22:01:09 +0100

    fixing other problems

--- src/pages/components/dashboard/DisplayDashboardTitleIssues.js
+++ src/pages/components/dashboard/DisplayDashboardTitleIssues.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const DisplayDashboardTitleIssues = () => (
-	<div className="grid grid-cols-6 p-4 bg-grey_light text-wildmine_black shadow-md rounded-t-lg font-bold mt-12 text-center text-base sm:text-xs">
+	<div className="grid grid-cols-7 p-4 bg-grey_light text-wildmine_black shadow-md rounded-t-lg font-bold mt-12 text-center text-base sm:text-xs">
 		<p>No du ticket</p>
 		<p>Priorité</p>
 		<p>Nom du ticket</p>
 		<p>Commentaire</p>
+		<p>Description</p>
 		<p>Statut</p>
 		<p>Date</p>
 	</div>
